c360efac091ba4efa2f8748d6518e31503d55987
Muhammad Saad <muhammadsaaddeveloper@gmail.com>
2023-10-27 07:03:03 -0700

    blogs added

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -7,54 +7,50 @@ import BlogDetails1 from "../../components/Blog-details/Blog1.jsx";
 import BlogDetails2 from "../../components/Blog-details/Blog2.jsx";
 import BlogDetails3 from "../../components/Blog-details/Blog3.jsx";
 import Footer from "../../components/Footer/footer";
-import Error from "../404";
 import BlogList from "../../pages/blog-list/blog-list-dark";
  
 const BlogDetailsDark = () => {
     const navbarRef = React.useRef(null);
     const logoRef = React.useRef(null);
+    const router = useRouter(); // call useRouter hook at the top of your component
+    const slug = router.query.slug; // you can store the slug in a variable for easier access
   
     React.useEffect(() => {
-      var navbar = navbarRef.current,
-        logo = logoRef.current;
-      if (window.pageYOffset > 300) {
-        navbar.classList.add("nav-scroll");
-      } else {
-        navbar.classList.remove("nav-scroll");
-      }
-      window.addEventListener("scroll", () => {
-        if (window.pageYOffset > 300) {
-          navbar.classList.add("nav-scroll");
-        } else {
-          navbar.classList.remove("nav-scroll");
-        }
-      });
+        var navbar = navbarRef.current;
+        const handleScroll = () => {
+            if (window.pageYOffset > 300) {
+                navbar.classList.add("nav-scroll");
+            } else {
+                navbar.classList.remove("nav-scroll");
+            }
+        };
+        
+        handleScroll(); // set the initial state
+        window.addEventListener("scroll", handleScroll);
+        
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
     }, [navbarRef]);
+    
     return (
-      <DarkTheme>
-        <div className="circle-bg">
-          <div className="circle-color fixed">
-            <div className="gradient-circle"></div>
-            <div className="gradient-circle two"></div>
-          </div>
-        </div>
-        <Navbar nr={navbarRef} lr={logoRef} />
-           
-           {/* condition for routes title set as per the route and data is set as per the route */}
-        {useRouter().query.slug === "Is-Business-Services-A-Good-Career-Path" ? (
-            <BlogDetails1 data={blog3Data} />
-            ) : useRouter().query.slug === "How-to-Be-Better-at-Digital-Marketing" ? (
-            <BlogDetails2 data={blog3Data} />
-            ) : useRouter().query.slug === "How-to-Stay-Relevant-in-the-Field-of-Software-Development" ? (
-            <BlogDetails3 data={blog3Data} />
+        <DarkTheme>
+            {/* ...other components... */}
+            <Navbar nr={navbarRef} lr={logoRef} />
+            
+            {slug === "Is-Business-Services-A-Good-Career-Path" ? (
+                <BlogDetails1 data={blog3Data} />
+            ) : slug === "How-to-Be-Better-at-Digital-Marketing" ? (
+                <BlogDetails2 data={blog3Data} />
+            ) : slug === "How-to-Stay-Relevant-in-the-Field-of-Software-Development" ? (
+                <BlogDetails3 data={blog3Data} />
             ) : (
-            <BlogList />
+                <BlogList />
             )}
 
-
             <Footer />
-      </DarkTheme>
+        </DarkTheme>
     );
-  };
+};
   
-  export default BlogDetailsDark;
+export default BlogDetailsDark;
